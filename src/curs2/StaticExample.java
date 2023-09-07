e8e05210ba7b868927c0f2fcc5dc449a844d1fc8
Dragos Tanta <dragos.tanta@gmail.com>
2023-09-07 20:45:57 +0300

    curs2 intermediar

---
+++ src/curs2/StaticExample.java
@@ -0,0 +1,31 @@
+package curs2;
+
+public class StaticExample {
+
+	String nume;
+	static String prenume;
+	
+	
+	String varsta;//declararea unei variabile
+	//varsta = "24";//initializarea variabilei
+	String masina = "Dacia";
+	
+	
+	public static void main(String[] args) {
+
+		StaticExample obj1 = new StaticExample();
+		obj1.nume = "Maria";
+		obj1.prenume = "Ioana";
+		
+		StaticExample obj2 = new StaticExample();
+		obj2.nume = "Alina";
+		obj2.prenume = "Carmen";
+		
+		
+		System.out.println(obj1.nume);
+		System.out.println(obj1.prenume);
+		
+		
+	}
+
+}
---
+++ src/curs2/Tester.java
@@ -0,0 +1,19 @@
+package curs2;
+
+public class Tester {
+	
+	//variabile
+	String nume;
+	int varsta;
+	int ratePerHour = 1;
+	
+	//constructori
+	//contructor default
+	//public Tester() {}
+	
+	public Tester(int salariuPeOra) {
+		ratePerHour = salariuPeOra;
+	}
+
+	//metode
+}
