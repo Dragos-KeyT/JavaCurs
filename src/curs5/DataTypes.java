c47556eb68f8646ed73da24bd7d259aaf4cc97bc
Dragos Tanta <dragos.tanta@gmail.com>
2023-09-18 20:32:55 +0300

    curs5 commit 2

--- src/curs5/DataTypes.java
+++ src/curs5/DataTypes.java
@@ -1,10 +1,22 @@
 package curs5;
 
 public class DataTypes {
-
+	
+	public short nrMeuShort;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		byte nrMeuByte = 127;
+		short nrMeuShort = 32000;
+		int nrMeuInteger = 2000000000;
+		System.out.println(nrMeuShort);
+		
+		String text = "mar";
+		
+		char m = 'm';
+		
+		
 	}
+	
 
 }
