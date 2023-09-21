a3644106a466fe091f836174d26c774631b0a286
Dragos Tanta <dragos.tanta@gmail.com>
2023-09-21 20:40:44 +0300

    curs6 intermediar

---
+++ src/curs6/BreakContinueExample.java
@@ -0,0 +1,19 @@
+package curs6;
+
+public class BreakContinueExample {
+
+	public static void main(String[] args) {
+
+		for(int i = 0; i<10; i++) {
+			
+			if(i == 4) {
+				//break;
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
---
+++ src/curs6/WhileExample.java
@@ -0,0 +1,19 @@
+package curs6;
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		
+		while(i >0) {
+			
+			System.out.println(i);
+			i--;
+			
+		}
+		
+		
+	}
+
+}
