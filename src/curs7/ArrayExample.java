226f559a27c11bb982fba97e46d38d98dcff68e9
Dragos Tanta <dragos.tanta@gmail.com>
2023-09-25 20:40:49 +0300

    curs7 intermediar

---
+++ src/curs7/ArrayExample.java
@@ -0,0 +1,42 @@
+package curs7;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+
+		String[] textArray = new String[4];
+	//	String[] textArray = {null, null, null, null}
+	//   index :                0     1    2      3	
+		System.out.println(textArray[0]);
+		textArray[0] = "this ";
+		System.out.println(textArray[0]);
+	//	String[] textArray = {this, null, null, null}
+		textArray[1] = "is ";
+	//	String[] textArray = {this, is, null, null}
+		System.out.println(textArray[1]);
+		textArray[2] = "an ";
+		textArray[3] = "array ";
+	//	String[] textArray = {this, is, an , array}
+		System.out.println(Arrays.toString(textArray));
+		
+		System.out.println("-----------------------------");
+		
+		for(int i = 0; i < textArray.length; i++) {
+			
+			System.out.print(textArray[i]);
+			
+		}
+		
+		System.out.println("\n-------------------------------");
+		//for each
+		
+		for(String element : textArray) {
+			
+			System.out.print(element);
+		}
+		
+	}
+
+}
---
+++ src/curs7/SwitchExample2.java
@@ -0,0 +1,51 @@
+package curs7;
+
+import java.util.Scanner;
+
+public class SwitchExample2 {
+/*
+ * Facem un program care verifica daca un student este elegibil pentru o bursa 
+ * Conditiile sunt :
+ * -sa fie anul 3
+ * - sa aiba un punctaj de 80 pentru bursa 1
+ * - sa aiba un punctaj de 50 pentru bursa 2
+ * 
+ * 
+ */
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Te rog introdu ce an esti :");
+		int an =  scan.nextInt();
+
+		
+		
+		switch(an) {
+		case 1:
+		case 2:
+			System.out.println("Nu esti eligibil");
+			break;
+		case 3:
+			System.out.println("Te rog introdu ce punctaj ai:");
+			int punctaj =  scan.nextInt();
+			switch(punctaj) {
+				case 80:
+					System.out.println("Eligibil pentru bursa 1");
+					break;
+				case 50:
+					System.out.println("Eligibil pentru bursa 2");
+					break;
+				default:
+					System.out.println("Te rog introdu un punctaj valid!");
+			}
+			break;
+		default:
+			System.out.println("Te rog sa introduci un an valid");
+		}
+		
+		
+		
+	}
+
+}
