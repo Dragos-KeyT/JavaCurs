6d0afc167070abaa563ed5cdd8021d87345c40d2
Dragos Tanta <dragos.tanta@gmail.com>
2023-09-28 21:30:25 +0300

    curs8 final

---
+++ src/curs8/Shape.java
@@ -0,0 +1,16 @@
+package curs8;
+
+public class Shape {
+	
+	String nume = "forma generica";
+	String culoare =  "culoare generica";
+	
+	public Shape(String culoare, String nume) {
+		this.culoare = culoare;
+		this.nume = nume;
+		
+	}
+	
+	
+
+}
