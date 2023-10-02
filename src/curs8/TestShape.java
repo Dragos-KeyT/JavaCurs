f055a1bcc908ab53cb6af1c61cbd08db13295640
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-02 20:38:06 +0300

    curs9 intermediar

--- src/curs8/TestShape.java
+++ src/curs8/TestShape.java
@@ -1,9 +1,11 @@
 package curs8;
 
+import curs9.MethodOverload;
+
 public class TestShape {
 
 	public static void main(String[] args) {
-
+		
 		Square patrat = new Square("Rosu", "Cerc");
 		
 		System.out.println(patrat.culoare);
---
+++ src/curs9/TestEncapsulare.java
@@ -0,0 +1,14 @@
+package curs9;
+
+public class TestEncapsulare {
+
+	public static void main(String[] args) {
+
+		ExampluEncapsulare  obj = new ExampluEncapsulare(-23);
+		//obj.numar = -23;
+		System.out.println(obj.getNumar());
+	
+		
+	}
+
+}
