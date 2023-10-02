cb0e14d3309286e3cccdc68f45edb9ff8b6cc0cb
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-02 21:26:02 +0300

    curs9 final

---
+++ src/curs9/MobilePhone.java
@@ -0,0 +1,11 @@
+package curs9;
+
+public class MobilePhone extends Product{
+
+	@Override
+	public int calculatePrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/curs9/WildAnimal.java
@@ -0,0 +1,11 @@
+package curs9;
+
+public class WildAnimal {
+	
+	public void makeSound() {
+		System.out.println("make an animal sound");
+	}
+	
+	
+
+}
